9c26a7d1a17d6d0c02e317336f885fffd43a4857
Aldranod <kurzydlo.b@gmail.com>
2018-09-30 18:21:52 +0200

    levels

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -9,6 +10,7 @@ public class Ball : MonoBehaviour {
 
     // state
     Vector2 paddleToBallVector;
+    bool hasStarted = false;
 
 	// Use this for initialization
 	void Start ()
@@ -19,7 +21,25 @@ public class Ball : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
+        if(!hasStarted)
+        {
+            LockBallToPaddle();
+            LaunchBallOnClick();
+        }
+    }
+
+    private void LaunchBallOnClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            hasStarted = true;
+            GetComponent<Rigidbody2D>().velocity = new Vector2(2f, 15f);
+        }
+    }
+
+    private void LockBallToPaddle()
+    {
         Vector2 paddlePos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
         transform.position = paddlePos + paddleToBallVector;
-	}
+    }
 }
